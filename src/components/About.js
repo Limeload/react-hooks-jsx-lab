706061985c10e1079c04b99b706fd164ddd0d279
Shraddha Rao <shraddharaom@gmail.com>
2022-09-05 03:46:21 +0530

    commit tree 834fb2975061681fb933279963c1ab40f6c80d48

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+<h2> About Me </h2>
+<p> Hello there, I am pleased to welcome you to my portfolio website where I showcase my projects!</p>
+<img src={image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
